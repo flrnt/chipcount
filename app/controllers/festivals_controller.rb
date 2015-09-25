69029a9c45204e7b4bc80888264d683e6c59ef56
Florent Allain <allain.florent@gmail.com>
2015-09-25 22:22:43 +0200

    add new/create action and private methods

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,8 +1,22 @@
 class FestivalsController < ApplicationController
+  before_action :set_festival, only: [:show]
   def index
     @festivals = Festival.all
   end
   def show
+
+  end
+  def new
+    @festival = Festival.new
+  end
+  def create
+    @festival = Festival.create(festival_params)
+  end
+  private
+  def set_festival
     @festival = Festival.find(params[:id])
+  end
+  def festival_params
+    params.require(:festival).permit(:name, :address, :start_date, :end_date)
   end
 end
